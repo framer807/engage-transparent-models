cd984ba5248c06fec14254ecf23387ce1fc2d2d7
framer807 <161201303+framer807@users.noreply.github.com>
2024-04-02 17:03:20 -0400

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+function main() {
+    const data = [];
+    return data;
+        console.log("Random Number: " + item);
+main();
+
+
+    }
+}
+    const data = generateRandomData();
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+}
+        int[] data = new int[10];
+public class RandomDataGenerator {
+}
+        }
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+
+
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+            System.out.println("Random Number: " + item);
+    }
